ac209ee6fda48a1f1773b226cfa0c805f31c0407
Ivádi Zsombor <zsombor.ivadi@student.uni-miskolc.hu>
2026-07-09 12:01:22 +0200

    feat(parser): add ThrowingErrorListener to handle syntax errors

---
+++ SqlToLinq.Cli/ThrowingErrorListener.cs
@@ -0,0 +1,27 @@
+using Antlr4.Runtime;
+using System;
+using System.IO;
+
+namespace SqlToLinq.Cli {
+
+    public class SqlSyntaxException : Exception {
+        public SqlSyntaxException(string message) : base(message) { }
+    }
+
+    public class ThrowingErrorListener : IAntlrErrorListener<IToken>, IAntlrErrorListener<int> {
+
+        public static readonly ThrowingErrorListener Instance = new ThrowingErrorListener();
+
+        public void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol,
+
+            int line, int charPositionInLine, string msg, RecognitionException e) {
+            throw new SqlSyntaxException($"[SQL syntax error] {line}. row, {charPositionInLine}. column: {msg}");
+        }
+
+        public void SyntaxError(TextWriter output, IRecognizer recognizer, int offendingSymbol,
+
+            int line, int charPositionInLine, string msg, RecognitionException e) {
+            throw new SqlSyntaxException($"[SQL lexer error] {line}. row, {charPositionInLine}. column: {msg}");
+        }
+    }
+}
